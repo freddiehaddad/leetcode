7e85d734dcb8af1c915c6a8593988fa59d03e0bf
Freddie Haddad <freddie.haddad@outlook.com>
2024-05-03 20:20:45 -0700

    26. Remove Duplicates from Sorted Array

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -34,6 +34,30 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 }
 
+// Given an integer array nums sorted in non-decreasing order, remove the
+// duplicates in-place such that each unique element appears only once. The
+// relative order of the elements should be kept the same. Then return the
+// number of unique elements in nums.
+//
+// Consider the number of unique elements of nums to be k, to get accepted, you
+// need to do the following things:
+//
+//   - Change the array nums such that the first k elements of nums
+//     contain the unique elements in the order they were present in
+//     nums initially. The remaining elements of nums are not important
+//     as well as the size of nums.
+//   - Return k.
+func removeDuplicates(nums []int) int {
+	k := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
+
 // Given an integer array nums and an integer val, remove all occurrences of
 // val in nums in-place. The order of the elements may be changed. Then return
 // the number of elements in nums which are not equal to val.
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -45,6 +45,43 @@ func TestMerge(t *testing.T) {
 	}
 }
 
+func TestRemoveDuplicates(t *testing.T) {
+	tests := []struct {
+		nums     []int
+		k        int
+		expected []int
+	}{
+		{
+			[]int{0},
+			1,
+			[]int{0},
+		},
+		{
+			[]int{1, 1, 2},
+			2,
+			[]int{1, 2},
+		},
+		{
+			[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			5,
+			[]int{0, 1, 2, 3, 4},
+		},
+	}
+
+	for i, test := range tests {
+		k := removeDuplicates(test.nums)
+		if test.k != k {
+			t.Errorf("[%d] length wrong. expected=%d got=%d",
+				i, test.k, k)
+		}
+		if slices.Compare(
+			test.nums[:test.k], test.expected) != 0 {
+			t.Errorf("[%d] result wrong. expected=%v got=%v",
+				i, test.expected, test.nums[:test.k])
+		}
+	}
+}
+
 func TestRemoveElement(t *testing.T) {
 	tests := []struct {
 		nums     []int
