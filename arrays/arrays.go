cfc0db8a8c57d508bb28330ff92c8680780badcf
Freddie Haddad <freddie.haddad@outlook.com>
2024-05-03 20:17:12 -0700

    88. Merge Sorted Array

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,1 +1,35 @@
 package arrays
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing
+// order, and two integers m and n, representing the number of elements in
+// nums1 and nums2 respectively.
+//
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//
+// The final sorted array should not be returned by the function, but instead
+// be stored inside the array nums1. To accommodate this, nums1 has a length of
+// m + n, where the first m elements denote the elements that should be merged,
+// and the last n elements are set to 0 and should be ignored. nums2 has a
+// length of n.
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m + n - 1
+	m--
+	n--
+
+	for m >= 0 && n >= 0 {
+		if nums1[m] > nums2[n] {
+			nums1[i] = nums1[m]
+			m--
+		} else {
+			nums1[i] = nums2[n]
+			n--
+		}
+		i--
+	}
+
+	for n >= 0 {
+		nums1[i] = nums2[n]
+		i--
+		n--
+	}
+}
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,1 +1,46 @@
 package arrays
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	tests := []struct {
+		nums1    []int
+		m        int
+		nums2    []int
+		n        int
+		expected []int
+	}{
+		{
+			[]int{1, 2, 3, 0, 0, 0},
+			3,
+			[]int{2, 5, 6},
+			3,
+			[]int{1, 2, 2, 3, 5, 6},
+		},
+		{
+			[]int{1},
+			1,
+			[]int{},
+			0,
+			[]int{1},
+		},
+		{
+			[]int{0, 0},
+			1,
+			[]int{1},
+			1,
+			[]int{0, 1},
+		},
+	}
+
+	for i, test := range tests {
+		merge(test.nums1, test.m, test.nums2, test.n)
+		if slices.Compare(test.nums1, test.expected) != 0 {
+			t.Errorf("[%d] result wrong. expected=%v got=%v",
+				i, test.expected, test.nums1)
+		}
+	}
+}
