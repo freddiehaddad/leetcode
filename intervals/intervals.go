08a992f4b6dff315eca4e6718bebdc2aaa47bbf1
Freddie Haddad <freddie.haddad@outlook.com>
2024-05-21 10:12:28 -0700

    435. Non-overlapping Intervals

--- intervals/intervals.go
+++ intervals/intervals.go
@@ -2,6 +2,54 @@ package intervals
 
 import "slices"
 
+// 435. Non-overlapping Intervals
+//
+// Given an array of intervals intervals where intervals[i] = [starti, endi],
+// return the minimum number of intervals you need to remove to make the rest
+// of the intervals non-overlapping.
+//
+// Example 1:
+//
+// Input: intervals = [[1,2],[2,3],[3,4],[1,3]] Output: 1 Explanation: [1,3]
+// can be removed and the rest of the intervals are non-overlapping.
+//
+// Example 2:
+//
+// Input: intervals = [[1,2],[1,2],[1,2]] Output: 2 Explanation: You need to
+// remove two [1,2] to make the rest of the intervals non-overlapping.
+//
+// Example 3:
+//
+// Input: intervals = [[1,2],[2,3]] Output: 0 Explanation: You don't need to
+// remove any of the intervals since they're already non-overlapping.
+//
+// Constraints:
+//
+//  1. 1 <= intervals.length <= 105
+//  2. intervals[i].length == 2
+//  3. -5 * 104 <= starti < endi <= 5 * 104
+func eraseOverlapIntervals(intervals [][]int) int {
+	slices.SortFunc(intervals, func(a, b []int) int {
+		if a[0] == b[0] {
+			return a[1] - b[1]
+		}
+		return a[0] - b[0]
+	})
+	var erase int
+	var previous int
+	for i := 1; i < len(intervals); i++ {
+		if intervals[previous][1] <= intervals[i][0] {
+			previous = i
+		} else {
+			erase++
+			if intervals[i][1] <= intervals[previous][1] {
+				previous = i
+			}
+		}
+	}
+	return erase
+}
+
 // 56. Merge Intervals
 //
 // Given an array of intervals where intervals[i] = [starti, endi], merge all
--- intervals/intervals_test.go
+++ intervals/intervals_test.go
@@ -5,6 +5,39 @@ import (
 	"testing"
 )
 
+func TestEraseOverlapIntervals(t *testing.T) {
+	tests := []struct {
+		input    [][]int
+		expected int
+	}{
+		{
+			[][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}},
+			1,
+		},
+		{
+			[][]int{{1, 2}, {1, 2}, {1, 2}},
+			2,
+		},
+		{
+			[][]int{{1, 2}, {2, 3}},
+			0,
+		},
+		{
+			[][]int{{1, 100}, {11, 22}, {1, 11}, {2, 12}},
+			2,
+		},
+	}
+
+	for i, test := range tests {
+		result := eraseOverlapIntervals(test.input)
+		if test.expected != result {
+			t.Errorf("[%d] result wrong. expected=%d got=%d",
+				i, test.expected, result,
+			)
+		}
+	}
+}
+
 func TestMerge(t *testing.T) {
 	tests := []struct {
 		input    [][]int
